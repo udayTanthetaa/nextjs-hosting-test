8441da5056241146d4384bd134519107016f9db8
Uday Tanthetaa <uday@tanthetaa.co.in>
2023-04-24 17:25:12 +0530

    updated 2

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 		<main className="flex flex-col items-center justify-between min-h-screen p-24">
 			<div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
 				<p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Updated &nbsp;
+					This has been Updated &nbsp;
 					<code className="font-mono font-bold">app/page.js</code>
 				</p>
 				<div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
